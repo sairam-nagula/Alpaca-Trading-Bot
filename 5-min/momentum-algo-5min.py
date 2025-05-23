fe1a3eadf6eb7e1da871ed340d2bdbe9a8324239
Sairam Nagula <sairamnagulaharshit@gmail.com>
2025-05-23 09:39:44 -0400

    Fix: Position size changed to 1000

--- 5-min/momentum-algo-5min.py
+++ 5-min/momentum-algo-5min.py
@@ -23,7 +23,7 @@ data_client = StockHistoricalDataClient(API_KEY, SECRET_KEY)
 
 # Strategy parameters
 MOMENTUM_THRESHOLD = 0.15  # in %
-POSITION_SIZE = 600  # dollars per trade
+POSITION_SIZE = 1000  # dollars per trade
 HOLD_DURATION_MINUTES = 15
 STOP_LOSS_PCT = -2.0  # in %
 
